f09d7ed85df53980754756e4d561786db23e548b
Brennan Holten <bholten@protonmail.ch>
2018-06-11 23:06:51 -0500

    pipenv in python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -4,15 +4,19 @@
               auto-mode-alist))
 
 (require-package 'pip-requirements)
+(require-package 'pipenv)
 
 (when (maybe-require-package 'anaconda-mode)
   (after-load 'python
     (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+    (add-hook 'python-mode-hook 'pipenv-mode))
   (when (maybe-require-package 'company-anaconda)
     (after-load 'company
       (add-hook 'python-mode-hook
                 (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
 
+(setq pipenv-projectile-after-switch-function
+      #'pipenv-projectile-after-switch-extended)
 
 (provide 'init-python)
