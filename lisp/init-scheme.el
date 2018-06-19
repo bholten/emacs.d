6f367efaf39949bb8942e04ab0a619c5c8286332
Brennan Holten <brennan@telsasoft.com>
2018-06-19 11:21:48 -0500

    Enabled Racket's unicode support.

--- lisp/init-scheme.el
+++ lisp/init-scheme.el
@@ -1,4 +1,12 @@
 (require-package 'racket-mode)
+(require-package 'scribble-mode)
 (require-package 'geiser)
+
+;; Set tab auto complete
+(setq tab-always-indent 'complete)
+
+;; Add unicode support for racket
+(add-hook 'racket-mode-hook      #'racket-unicode-input-method-enable)
+(add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
 
 (provide 'init-scheme)
