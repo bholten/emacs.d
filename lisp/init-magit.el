bfc8fbea1237d75c5ee21903f18cf5158f0afe0e
Brennan Holten <bholten@protonmail.ch>
2022-09-01 00:33:07 -0500

    Add git-identity

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -10,5 +10,30 @@
 ;; Bind the `magit-status' command to a convenient key.
 (global-set-key (kbd "C-c g") #'magit-status)
 
+
+(unless (package-installed-p 'magit-lfs)
+  (package-install 'magit-lfs))
+
+(require 'magit-lfs)
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(use-package git-identity
+  :after magit
+  :config
+  (git-identity-magit-mode 1)
+  ;; Bind I to git-identity-info in magit-status
+  (define-key magit-status-mode-map (kbd "I") 'git-identity-info)
+  :custom
+  ;; Warn if the global identity setting violates your policy
+  (git-identity-verify t)
+  ;; The default user name
+  (git-identity-default-username "Brennan Holten"))
+
+;; And set git-identity-list in your custom-file or init file
+(load-file (expand-file-name "lisp/.git-identities.el" user-emacs-directory))
+(setq git-identity-list *identity-list*)
+
 (provide 'init-magit)
 ;;; init-magit.el ends here
