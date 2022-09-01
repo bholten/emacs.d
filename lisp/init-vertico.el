6715e775f801c8fbb04d5b9ed9898c6a54d63f48
Brennan Holten <bholten@protonmail.ch>
2022-09-01 05:08:45 -0500

    Added orderless to vertico

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -15,5 +15,17 @@
   (define-key vertico-map (kbd "DEL") #'vertico-directory-delete-word)
   (define-key vertico-map (kbd "M-d") #'vertico-directory-delete-char))
 
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
 (provide 'init-vertico)
 ;;; init-vertico.el ends here
