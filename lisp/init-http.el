fbf4eacb80198247d50bdbd6cff0ca90cd05f1b5
Brennan Holten <bholten@protonmail.ch>
2022-09-06 12:58:36 -0500

    Added my old print-to-browser htmlize function

--- lisp/init-http.el
+++ lisp/init-http.el
@@ -11,6 +11,9 @@
 (unless (package-installed-p 'company-restclient)
   (package-install 'company-restclient))
 
+(unless (package-installed-p 'htmlize)
+  (package-install 'htmlize))
+
 (add-hook 'restclient-mode 'company-restclient)
 
 (defun bholten/restclient ()
@@ -18,6 +21,10 @@
   (with-current-buffer (cvs-get-buffer-create "*restclient*")
     (restclient-mode)
     (pop-to-buffer (current-buffer))))
+
+(defun bholten/print-to-browser ()
+  (interactive)
+  (browse-url-of-buffer (htmlize-buffer)))
 
 (provide 'init-http)
 ;;; init-http.el ends here
