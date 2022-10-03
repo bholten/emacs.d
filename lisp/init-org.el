47169260a2374f9c6ddedf2013e2c8a4a89c1b67
Brennan Holten <bholten@protonmail.ch>
2022-10-03 15:40:46 -0500

    ox-reveal for org-mode presentations

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -8,6 +8,10 @@
 
 ;;; Additional Org-mode related functionality
 (use-package org-contrib)
+;;; Reveal.js presentations
+(use-package ox-reveal)
+
+(add-hook 'org-mode-hook (lambda () (load-library "ox-reveal")))
 
 (provide 'init-org)
 ;;; init-org.el ends here
