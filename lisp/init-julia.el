a94aae0a0bb1b37c61a82cf122161574abf6f467
Brennan Holten <bholten@protonmail.ch>
2024-02-10 11:15:19 -0600

    Add Julia repl

--- lisp/init-julia.el
+++ lisp/init-julia.el
@@ -2,7 +2,19 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package julia-mode)
+(use-package julia-mode
+  :ensure t)
+
+(use-package julia-repl
+  :ensure t
+  :hook (julia-mode . julia-repl-mode)
+
+  :init
+  (setenv "JULIA_NUM_THREADS" "8")
+
+  :config
+  ;; Set the terminal backend
+  (julia-repl-set-terminal-backend 'vterm))
 
 (provide 'init-julia)
 ;;; init-julia.el ends here
